d78985150f28f8f82275da6a269f1cdc2c57629f
Nick Flynn <nickrflynn@gmail.com>
2017-10-25 12:23:15 -0600

    Cleanup work and dark mode

--- src/app/trip-chart/trip-chart.component.ts
+++ src/app/trip-chart/trip-chart.component.ts
@@ -8,24 +8,37 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 })
 export class TripChartComponent implements OnInit {
 
+  private chartData: any;
+  private labels: Array<string> = [];
+
   constructor( @Inject(MAT_DIALOG_DATA) public data: any) {
     console.log(data);
+    this.chartData = data;
+    console.log(this.getLabels());
   }
 
   ngOnInit() {
+  }
+
+  private getLabels() {
+    this.chartData.trips.forEach(element => {
+      if (!this.labels.includes(element.year)) {
+        this.labels.push(element.year);
+      }
+    });
   }
 
   public barChartOptions:any = {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels:string[] = ['2015', '2016', '2017'];
   public barChartType:string = 'bar';
   public barChartLegend:boolean = true;
  
   public barChartData:any[] = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {data: [65, 59, 80], label: 'Miles Driven'},
+    {data: [28, 48, 40], label: 'Miles Flown'}
   ];
  
   // events
@@ -41,12 +54,8 @@ export class TripChartComponent implements OnInit {
     // Only Change 3 values
     let data = [
       Math.round(Math.random() * 100),
-      59,
-      80,
       (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40];
+      (Math.random() * 100)];
     let clone = JSON.parse(JSON.stringify(this.barChartData));
     clone[0].data = data;
     this.barChartData = clone;
