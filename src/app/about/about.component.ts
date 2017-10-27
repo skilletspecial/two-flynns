89d3c2ce2685a933a0d00adc31a473827c4df1ab
Nick Flynn <nickrflynn@gmail.com>
2017-10-26 22:27:09 -0600

    Update about dialog stats

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,12 +9,26 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 export class AboutComponent implements OnInit {
 
   public destinations: Array<any>;
+  public milesTraveled: number;
+  public bathroomBreaks: number;
 
   constructor( @Inject(MAT_DIALOG_DATA) public data: any) {
     this.destinations = data.trips;
   }
 
   ngOnInit() {
+    this.milesTraveled = this.countMilesTraveled();
+    this.bathroomBreaks = Math.round((this.milesTraveled/400));
+  }
+
+  private countMilesTraveled(): number {
+    let sum = 0;
+    this.destinations.forEach(destination => {
+      sum += destination.milesDriven;
+      sum += destination.milesFlown;
+    });
+
+    return sum;
   }
 
 }
